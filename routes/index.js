fe2f08e57866981bdc45a55af36c95ddb1f846e5
heon <caqudgjs@naver.com>
2023-11-23 16:31:55 +0900

    choi
    plus restaurant selection

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,26 @@ const sectionIcons = [
   '🍚', '🍿', '🍜', '🍣', '🥩', '☕', '🍰'
 ]
 
+const statusKorMap = {
+  OPN: '영업중', 
+  CLS: '폐업',
+  VCT: '휴가중',
+  RMD: '리모델링'
+}
+
+router.get('/biz-simple', async function(req, res, next) {
+  const businesses = await sql.getBusinessesJoined(req.query)
+  businesses.map((item) => {
+    item.status_kor = statusKorMap[item.status]
+    return item
+  })
+
+  res.render('biz-simple', { 
+    title: '단순 식당 목록',
+    businesses
+  });
+});
+
 router.get('/', async function(req, res, next) {
 
   const sections = await sql.getSections()
@@ -16,6 +36,20 @@ router.get('/', async function(req, res, next) {
   res.render('sections', { 
     title: '섹션 목록',
     sections
+  });
+});
+
+router.get('/biz-adv', async function(req, res, next) {
+  const businesses = await sql.getBusinessesJoined(req.query)
+  businesses.map((item) => {
+    item.status_kor = statusKorMap[item.status]
+    return item
+  })
+
+  res.render('biz-adv', { 
+    title: '고급 식당 목록',
+    q: req.query,
+    businesses
   });
 });
 
