fe78daf28b0144dd9386f1bea35a4a5fbd21223e
heon <caqudgjs@naver.com>
2023-11-24 11:32:31 +0900

    Choi
    update like

--- routes/index.js
+++ routes/index.js
@@ -39,18 +39,54 @@ router.get('/', async function(req, res, next) {
   });
 });
 
+
 router.get('/biz-adv', async function(req, res, next) {
   const businesses = await sql.getBusinessesJoined(req.query)
   businesses.map((item) => {
     item.status_kor = statusKorMap[item.status]
     return item
   })
-
+  
+  
   res.render('biz-adv', { 
     title: '고급 식당 목록',
     q: req.query,
     businesses
   });
+});
+
+router.get('/business/:id', async function(req, res, next) {
+  const biz = await sql.getSingleBusinessJoined(req.params.id)
+  biz.status_kor = statusKorMap[biz.status]
+  biz.icon = sectionIcons[biz.section_id - 1]
+
+  const menus = await sql.getMenusOfBusiness(req.params.id)
+  const ratings = await sql.getRatingsOfBusiness(req.params.id)
+
+  res.render('detail', { 
+    biz,
+    menus,
+    ratings
+  });
+});
+
+router.put('/menus/:id', async function(req, res, next) {
+  const result = await sql.updateMenuLikes(req.params.id, req.body.like)
+  res.send(result)
+});
+
+router.post('/ratings', async function(req, res, next) {
+  const result = await sql.addRating(
+    req.body.business_id,
+    req.body.stars,
+    req.body.comment
+  )
+  res.send(result)
+});
+
+router.delete('/ratings/:id', async function(req, res, next) {
+  const result = await sql.removeRating(req.params.id)
+  res.send(result)
 });
 
 module.exports = router;
